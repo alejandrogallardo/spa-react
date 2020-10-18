c6386adaa71020713922ae3afc7389a7245b8be5
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2020-10-18 13:47:05 -0600

    Se agrego el router y la estructura inicial de directorios

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+  } from "react-router-dom";
+import { LoginScreen } from '../components/login/LoginScreen';
+import { MarvelScreen } from '../components/marvel/MarvelScreen';
+import { Navbar } from '../components/ui/Navbar';
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <div>
+                <Navbar />
+                <Switch>
+                    <Route exact path="/login" component={ LoginScreen } />
+                    <Route exact path="/" component={ MarvelScreen } />
+                </Switch>
+            </div>
+        </Router>
+    )
+}
