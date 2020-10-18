4a2ea65e05cc9c343e07742d7648455e279b0dfb
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2020-10-18 13:58:10 -0600

    Se agrego un segundo router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,17 +5,15 @@ import {
     Route,
   } from "react-router-dom";
 import { LoginScreen } from '../components/login/LoginScreen';
-import { MarvelScreen } from '../components/marvel/MarvelScreen';
-import { Navbar } from '../components/ui/Navbar';
+import { DashboardRoutes } from './DashboardRoutes';
 
 export const AppRouter = () => {
     return (
         <Router>
             <div>
-                <Navbar />
                 <Switch>
                     <Route exact path="/login" component={ LoginScreen } />
-                    <Route exact path="/" component={ MarvelScreen } />
+                    <Route path="/" component={ DashboardRoutes } />
                 </Switch>
             </div>
         </Router>
