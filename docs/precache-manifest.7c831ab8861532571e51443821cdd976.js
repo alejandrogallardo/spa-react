cc477209c26830509f7cca67fbf7f1f68c541f83
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2020-10-20 12:16:24 -0600

    se creo el build de produccion

---
+++ docs/precache-manifest.7c831ab8861532571e51443821cdd976.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b579a35049b6f06e583fd992d88a03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11a0fbcfbd55dc0642f8",
+    "url": "/static/js/2.6b40a01b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6b40a01b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7599426129104f4b9559",
+    "url": "/static/js/main.93e0f2ad.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
