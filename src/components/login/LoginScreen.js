49a2e3cb98680315282fa61a3db096ac1ca87958
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2020-10-19 19:09:35 -0600

    se creo la funcionalidad de login y logout (sin backend)

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,24 +1,50 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const LoginScreen = ({ history }) => {
 
+    const {dispatch} = useContext(AuthContext);
+
     const handleLogin = () => {
-        console.log('Holis');
+        // console.log('Holis');
         // history.push('/');
+        // history.replace('/');
+        dispatch({
+            type: types.login,
+            payload: {
+                name: 'Alejandro'
+            }
+        }) 
         history.replace('/');
     }
 
     return (
         <div className="container mt-5">
-            <h1>Login</h1>
+            <h1 className="text-center">Login</h1>
             <hr />
 
-            <button
-                className="btn btn-success"
-                onClick={handleLogin}
-            >
-                Login
-            </button>
+            <div className="row d-flex justify-content-center">
+                <div className="col-md-4 col-sm-12">
+                    <form>
+                        <div className="form-group">
+                            <label>Usuario:</label>
+                            <input className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label>Contraseña:</label>
+                            <input className="form-control"/>
+                        </div>
+                    </form>
+                    <button
+                        className="btn btn-success btn-block"
+                        onClick={handleLogin}
+                    >
+                        Login
+                    </button>
+                </div>
+            </div>
+           
         </div>
     )
 }
